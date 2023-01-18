dbe024b76539bac537a20d193352f2bca61da505
volodia <volodia@debian.lan>
2023-01-19 00:11:01 +0300

    first commit

---
+++ pointSmartHome.ino
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+#include <SPI.h>
+#include <mcp2515.h>
+//The setup function is called once at startup of the sketch
+void setup()
+{
+	int test;
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
