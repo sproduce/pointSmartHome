40acac1e93c07446ff7d4d6c9cfa84935d0e4297
volodia <volodia@debian.lan>
2023-01-21 19:30:24 +0300

    send random byte

--- pointSmartHome.ino
+++ pointSmartHome.ino
@@ -14,6 +14,7 @@ void setup()
 	canMsgSwitch.can_id = 0x0F6;
 	canMsgSwitch.can_dlc = 2;
 	canMsgSwitch.data[0] = 1;// current point for test. !!! read from EEPROM
+	canMsgSwitch.data[1] = switchPosition;
 
 	mcp2515.reset();
 	mcp2515.setBitrate(CAN_125KBPS);
@@ -24,6 +25,7 @@ void setup()
 void loop()
 {
 	mcp2515.sendMessage(&canMsgSwitch);
-
+	canMsgSwitch.data[1] = rand();
 	delay(1000);
+
 }
