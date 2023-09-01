3b5762b31f989b66315990ae303558f75ef9a101
volodia <volodia@debian.lan>
2023-09-02 01:55:24 +0300

    learn step

--- pointSmartHome.ino
+++ pointSmartHome.ino
@@ -5,79 +5,49 @@
 #include <mcp2515.h>
 //https://stackoverflow.com/questions/47981/how-do-i-set-clear-and-toggle-a-single-bit
 
-uint8_t switchPosition = 0b00000000;
-uint32_t canIdSystem = 0x81;// служебные сообщения
-uint32_t canIdSwitch = 0x82;// канал о состоянии выключателей
+#define COUNT_BUTTON 8
+
+
+
 struct can_frame canMsg;
+volatile bool canReceived = false;
+
+
+
+struct button {
+	uint8_t channel = 0;
+	uint8_t status = false;
+	uint32_t changeTime = 0;
+} buttons[COUNT_BUTTON];
+
 
 MCP2515 mcp2515(10);
 
-uint8_t oldKey1 = 1,oldKey2 = 1,key1,key2;
-uint8_t switchArray[] ={2,3,4,5,6,7,8,9};
-uint8_t pointId;
 
+void buttonsInit(){
 
-
-uint8_t getPointId(void)
-{
-	return EEPROM.read(0);
+	for(uint8_t i = 0; i < COUNT_BUTTON; i++){
+		if (buttons[i].channel){
+			pinMode(buttons[i].channel , INPUT_PULLUP);
+		}
+	}
 }
+
 
 
 
 void setup()
 {
 	mcp2515.reset();
-	mcp2515.setBitrate(CAN_125KBPS);
+	mcp2515.setBitrate(CAN_125KBPS, MCP_8MHZ);
 	mcp2515.setNormalMode();
-
-	pointId = getPointId();
-
-	for(uint8_t i : switchArray)
-	{
-		pinMode(i,INPUT_PULLUP);
-	}
-}
-
-
-void sendMessage(uint8_t currentStatus)
-{
-	canMsg.can_id = canIdSwitch;
-	canMsg.can_dlc = 2;
-	canMsg.data[0] = pointId;
-	canMsg.data[1] = currentStatus;
-	mcp2515.sendMessage(&canMsg);
+	Serial.begin(9600);
 }
 
 
 void loop()
 {
-	key1 = digitalRead(2);
-	key2 = digitalRead(3);
-	if (oldKey1 != key1){
-		if (key1){
-			switchPosition &= ~(1UL << 7);
-		} else {
-			switchPosition |= 1UL << 7;
-		}
-		oldKey1 = key1;
-		sendMessage(switchPosition);
-	}
 
-	if (oldKey2 != key2){
-		if (key2){
-			switchPosition &= ~(1UL << 6);
-		} else {
-			switchPosition |= 1UL << 6;
-		}
-		oldKey2 = key2;
-		sendMessage(switchPosition);
-	}
-delay(100);
 
-//	mcp2515.sendMessage(&canMsgSwitch);
-//	//canMsgSwitch.data[1] = rand();
-//	canMsgSwitch.data[1] = switchPosition;
-//	delay(1000);
 
 }
